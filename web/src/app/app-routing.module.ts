49c4bfd3691b50612c897c064a8d60275190015a
Brayan <brayan.11fera@gmail.com>
2023-02-24 17:33:27 +0500

    Menu para navegar entre telas

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './views/calculator/calculator.component';
+import { HomeComponent } from './views/home/home.component';
+import { MethodsComponent } from './views/methods/methods.component';
+import { TipsComponent } from './views/tips/tips.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'methods',
+    component: MethodsComponent,
+  },
+  {
+    path: 'tips',
+    component: TipsComponent,
+  },
+  {
+    path: 'calculator',
+    component: CalculatorComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppIconComponent } from './components/app-icon/app-icon.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CalculatorComponent } from './views/calculator/calculator.component';
+import { HomeComponent } from './views/home/home.component';
+import { MethodsComponent } from './views/methods/methods.component';
+import { TipsComponent } from './views/tips/tips.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    AppIconComponent,
+    HeaderComponent,
+    CalculatorComponent,
+    HomeComponent,
+    MethodsComponent,
+    TipsComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
