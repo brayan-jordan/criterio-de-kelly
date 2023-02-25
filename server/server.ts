803b0ce3fb7b5f3a29b7b30b63880efee07d03f4
Brayan <brayan.11fera@gmail.com>
2023-02-25 10:23:33 +0500

    add cors to server

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient();
 
